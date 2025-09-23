1e20e69b007e2a8e56352016bd11b5a982f74d07
Rethabile Ntsekhe <rethabile.dev@gmail.com>
2025-09-23 15:00:16 +0000

    this is my old layout ``` // app/layout.tsx
    import './globals.css';
    im

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 
 import type {Metadata} from 'next';
 import './globals.css';
-import Header from '@/components/header';
 import { Providers } from './providers';
 import { Inter } from 'next/font/google';
 import { ToastProvider } from '@/components/ui/Toast';
@@ -38,7 +37,6 @@ export default function RootLayout({
       <body className="antialiased">
         <Providers>
           <ToastProvider>
-            <Header />
             <main>{children}</main>
           </ToastProvider>
         </Providers>
